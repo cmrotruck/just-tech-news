5c3376097e17f66cc1ee69fcdcc4bae8ca3951ee
cmrotruck <crotruck@gmail.com>
2022-01-30 17:23:08 -0500

    added comment modal

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,12 @@
-//import the Sequalize constructor from the library
 const Sequelize = require('sequelize');
 
-//pull in the .env config file
 require('dotenv').config();
 
-//create the connection to our database, pass in your MySQL information for usename and password
+// create connection to our db
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
+  host: 'localhost',
+  dialect: 'mysql',
+  port: 3306
 });
 
 module.exports = sequelize;
